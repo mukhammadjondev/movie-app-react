24e3261b9a9f3263cc7fdbc081aee807c04b21e4
mukhammadjondev <muhsdev@gmail.com>
2023-08-01 21:40:48 +0500

    Movie app website structure with react.js

---
+++ src/components/movie-list/movie-list.js
@@ -0,0 +1,16 @@
+import './movie-list.css'
+
+import MovieListItem from '../movie-list-item/movie-list-item'
+
+const MovieList = () => {
+  return (
+    <div className='movie-list'>
+        <MovieListItem />
+        <MovieListItem />
+        <MovieListItem />
+        <MovieListItem />
+    </div>
+  )
+}
+
+export default MovieList
---
+++ src/components/movies-add-form/movies-add-form.js
@@ -0,0 +1,16 @@
+import './movies-add-form.css'
+
+const MoviesAddForm = () => {
+  return (
+    <div className='movies-add-form'>
+        <h3>Yangi kinolar qo'shish</h3>
+        <form className='add-form d-flex'>
+            <input type="text" className='form-control new-post-label' placeholder='Qanday kino?' />
+            <input type="number" className='form-control new-post-label' placeholder="Nechi marotaba ko'rilgan" />
+            <button type='submit' className='btn btn-outline-dark'>Qo'shish</button>
+        </form>
+    </div>
+  )
+}
+
+export default MoviesAddForm
